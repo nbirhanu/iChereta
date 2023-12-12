95f2628d1344c9f57846ed5b7c756230045c4d93
nbirhanu <nbirhanu80@gmail.com>
2023-12-12 21:27:22 +0300

    build pricing page

---
+++ src/BeforeCreateAccount/pages/PricingPage.js
@@ -0,0 +1,13 @@
+import Footer from '../../components/Footer'
+import Pricing from '../../components/Pricing'
+
+function PricingPage() {
+	return (
+		<div style={{ marginTop: '14rem' }}>
+			<Pricing />
+			<Footer />
+		</div>
+	)
+}
+
+export default PricingPage
