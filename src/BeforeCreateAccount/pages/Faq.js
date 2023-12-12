286fbba0d72e7619c7fc35cfb93c174cd884cd2a
nbirhanu <nbirhanu80@gmail.com>
2023-12-12 21:23:44 +0300

    build FAQ page

--- src/BeforeCreateAccount/pages/Faq.js
+++ src/BeforeCreateAccount/pages/Faq.js
@@ -1,5 +1,13 @@
+import Footer from '../../components/Footer'
+import QuestionAnswer from '../../components/QuestionAnswer'
+
 function Faq() {
-	return <div></div>
+	return (
+		<div style={{ marginTop: '8rem' }}>
+			<QuestionAnswer />
+			<Footer />
+		</div>
+	)
 }
 
 export default Faq
