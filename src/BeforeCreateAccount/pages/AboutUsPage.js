f163fdfed6a826eed5fe4061b75d82ef6d0eff95
nbirhanu <nbirhanu80@gmail.com>
2023-12-12 21:25:28 +0300

    build about page

---
+++ src/BeforeCreateAccount/pages/AboutUsPage.js
@@ -0,0 +1,13 @@
+import About from '../../components/About'
+import Footer from '../../components/Footer'
+
+function AboutUsPage() {
+	return (
+		<div style={{ marginTop: '10rem' }}>
+			<About />
+			<Footer />
+		</div>
+	)
+}
+
+export default AboutUsPage
