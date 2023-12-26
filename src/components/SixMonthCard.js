432079c75b2552b67539634d02833aa69384af29
nbirhanu <nbirhanu80@gmail.com>
2023-12-26 21:57:59 +0300

    six month card

--- src/components/SixMonthCard.js
+++ src/components/SixMonthCard.js
@@ -8,10 +8,18 @@ import {
 	ListWrapper,
 	ThreeMonthTittle,
 	ThreeMonthSub,
+	UlSplite,
+	Ul2Splite,
+	ShowLess,
 } from './ThreeMonthCard'
+import { useState } from 'react'
 //
 
 function SixMonthCard() {
+	const [show, setShow] = useState(false)
+	const handleShow = function () {
+		setShow(!show)
+	}
 	return (
 		<ThreeMonthCardWrapper>
 			<ThreeMonthBotton>
@@ -23,38 +31,43 @@ function SixMonthCard() {
 				</Link>
 			</ThreeMonthBotton>
 			<ListWrapper>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>Unlimited Tender Access</Li>
-				</Ul>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>Daily Email Alert</Li>
-				</Ul>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>Daily SMS Alert</Li>
-				</Ul>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>Daily Telegram Notification</Li>
-				</Ul>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>Save Tenders</Li>
-				</Ul>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>4 Languages</Li>
-				</Ul>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>From All Over Ethiopia</Li>
-				</Ul>
-				<Ul>
-					<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
-					<Li style={{ fontSize: '1.1rem' }}>Support Service 24/7</Li>
-				</Ul>
+				<UlSplite>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>Unlimited Tender Access</Li>
+					</Ul>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>Daily Email Alert</Li>
+					</Ul>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>Daily SMS Alert</Li>
+					</Ul>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>Daily Telegram Notification</Li>
+					</Ul>
+				</UlSplite>
+				<Ul2Splite showmore={show}>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>Save Tenders</Li>
+					</Ul>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>4 Languages</Li>
+					</Ul>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>From All Over Ethiopia</Li>
+					</Ul>
+					<Ul>
+						<CheakedIcon src={Cheak} alt="list icon" style={{ width: '1.3rem' }} />
+						<Li style={{ fontSize: '1.1rem' }}>Support Service 24/7</Li>
+					</Ul>
+				</Ul2Splite>
+				<ShowLess onClick={handleShow}>{show ? 'Show less' : 'Show more'}</ShowLess>
 			</ListWrapper>
 		</ThreeMonthCardWrapper>
 	)
